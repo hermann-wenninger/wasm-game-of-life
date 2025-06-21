002d2762ecb10822c5349061c2e5568e9cefccad
wennhmann <info@wennhmann.de>
2025-06-21 10:47:17 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name:&str ) {
+    alert(&format!("Hello, {}!", name));
+}
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+
+struct Universe {
+    height:u32,
+    width:u32,
+    Cell:Vec<Cell>,
+
+
+}
+
+#[wasm_bindgen]
+pub fn create_raster(){}
